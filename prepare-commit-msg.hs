bbe25810f62313936f60bd1adf679e18541fa66d
Samuel Ytterbrink <neppord@gmail.com>
2019-05-22 14:03:10 +0200

    get lines added and removed.

--- prepare-commit-msg.hs
+++ prepare-commit-msg.hs
@@ -2,12 +2,33 @@
 -- stack script --resolver lts-13.20
 
 import System.Environment
+import System.Process
+import Data.Monoid
+
 import Language.Java.Parser
 
 main = do
     args <- getArgs
     go args
 
-go [commit_msg_file] = print commit_msg_file
+added :: String -> Integer
+added line = read $ words line !! 0
+
+removed :: String -> Integer
+removed line = read $ words line !! 1
+
+parseNumstat :: String -> (Integer, Integer)
+parseNumstat out = (getSum $ fst sumOfChanges, getSum $ snd sumOfChanges)
+    where
+        lines' = lines out
+        parseLine line = (Sum $ added line, Sum $ removed line)
+        listOfChanges = map parseLine  lines'
+        sumOfChanges = foldr (<>) (Sum 0, Sum 0) listOfChanges
+
+go [commit_msg_file] = do
+    out <- readProcess "git" ["diff", "-w", "--numstat", "HEAD"] ""
+    print $ parseNumstat out
+    print commit_msg_file
+
 go _ = return ()
 
